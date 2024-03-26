309f51a5663f346b75994ff9100c20692a78ea8b
Harrison Bouche <harrison@sm.media>
2024-03-26 09:22:33 -0400

    chore: updated deps, removed vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config"
 
-import vue from "@astrojs/vue"
-
 // https://astro.build/config
 export default defineConfig({
-	integrations: [vue()],
 	site: "https://uplionsserve.org",
 	base: "/diabetes-awareness-and-action",
 	output: "static",
